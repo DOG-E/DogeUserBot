5e07157aacd6490e604c3f646d282b9455401856
TeleDoge <DogeUserBot@gmail.com>
2021-09-11 15:39:22 +0000

    🦴 Auto Fixes
    Signed-off-by: TeleDoge <DogeUserBot@gmail.com>

--- userbot/core/cmdinfo.py
+++ userbot/core/cmdinfo.py
@@ -41,9 +41,7 @@ def _format_about(
         del about["header"]
 
     if "description" in about and isinstance(about["description"], str):
-        tmp_chelp += (
-            f"\n\n**🐾 Description:**\n" f"__{get_data(about, 'description')}__"
-        )
+        tmp_chelp += f"\n\n**🐾 Description:**\n" f"__{get_data(about, 'description')}__"
         del about["description"]
 
     if "flags" in about:
