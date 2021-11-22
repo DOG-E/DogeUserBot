7e1c17bb04a1f1ef1f35c892d34299a3d896233c
MUTLCC <mutluis@outlook.com>
2021-11-22 23:10:04 +0300

    .

--- userbot/languages/languages.py
+++ userbot/languages/languages.py
@@ -11,42 +11,48 @@
 from os import listdir, path
 from typing import Any, Dict, List, Union
 
-from googletrans import Translator
+from google_trans_new import google_translator
 from yaml import safe_load
 
 from ..sql_helper.globals import gvar
 
-languages = {}
-languages_folder = path.join(path.dirname(path.realpath(__file__)), "languages")
-Translate = Translator()
+language = [gvar("DOGELANG") or "en"]
+listlangs = {}
 
+Trs = google_translator()
+
+languages_folder = path.join(path.dirname(path.realpath(__file__)), "languages")
 
 for file in listdir(languages_folder):
     if file.endswith(".yml"):
         code = file[:-4]
-        languages[code] = safe_load(
+        listlangs[code] = safe_load(
             open(path.join(languages_folder, file), encoding="UTF-8"),
         )
 
 
 def lan(key: str) -> Any:
+    lang = language[0]
     try:
-        return languages[(gvar("DOGELANG") or "en")][key]
-
+        return listlangs[lang][key]
     except KeyError:
         try:
-            return Translate.translate(languages["en"][key], dest=gvar("DOGELANG")).text
-
+            trt = Trs.translate(listlangs["en"][key], lang_tgt=lang)
+            if listlangs.get(lang):
+                listlangs[lang][key] = trt
+            else:
+                listlangs.update({lang:{key:trt}})
+            return trt
         except KeyError:
-            return f"🚧 WARNING: Couldn't load any language with the key {key}"
+            return f"🚧 WARNING: Couldn't load any language with the key `{key}`"
 
 
 def lngs() -> Dict[str, Union[str, List[str]]]:
     return {
         code: {
-            "name": languages[code]["name"],
-            "natively": languages[code]["natively"],
-            "authors": languages[code]["authors"],
+            "name": listlangs[code]["name"],
+            "natively": listlangs[code]["natively"],
+            "authors": listlangs[code]["authors"],
         }
-        for code in languages
+        for code in listlangs
     }
