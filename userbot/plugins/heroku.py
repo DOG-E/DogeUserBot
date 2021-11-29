05e2f90a461eca78e57913b054bc6eb26aeefc1d
MUTLCC <mutluis@outlook.com>
2021-11-29 16:28:24 +0300

    a3

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -26,7 +26,7 @@ disable_warnings(InsecureRequestWarning)
 
 
 @doge.bot_cmd(
-    pattern="([Ss]et|[Gg]et|[Dd]el)[Vv]ar ([\s\S]*)",
+    pattern="([Ss]et|[Gg]et|[Dd]el) [Vv]ar ([\s\S]*)",
     command=("var", plugin_category),
     info={
         "h": "To manage heroku vars.",
