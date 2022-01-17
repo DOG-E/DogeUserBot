2327ff5a6faf3386d9cf757414c1ff8b55acd47c
aylak-github <yk3514003@gmail.com>
2022-01-17 14:45:40 +0300

    Test 38 By G4rip

--- userbot/assistant/botpm.py
+++ userbot/assistant/botpm.py
@@ -85,7 +85,7 @@ async def check_bot_started_users(user, event):
 
 
 @doge.shiba_cmd(
-    pattern=f"/start ?(.*)",
+    pattern=f"/(start|ba[sş]lat) ?(.*)",
     incoming=True,
     func=lambda e: e.is_private,
 )
